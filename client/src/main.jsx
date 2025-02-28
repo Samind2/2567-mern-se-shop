e45d4333187186b701ede2fb0c61057043c25a0e
Samind2 <suphaneerangsirat@gmail.com>
2025-02-28 20:46:13 +0700

    Up-Animation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense  } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import router from "./routes/Router";
@@ -6,12 +6,13 @@ import { RouterProvider } from "react-router";
 import AuthProvider from "./context/AuthContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CookiesProvider } from "react-cookie";
-
+import SuspenseContent from "./components/SuspenseContent";
 // Create a client
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+     <Suspense fallback={<SuspenseContent />}>
     <CookiesProvider>
       <AuthProvider>
         <QueryClientProvider client={queryClient}>
@@ -19,5 +20,6 @@ createRoot(document.getElementById("root")).render(
         </QueryClientProvider>
       </AuthProvider>
     </CookiesProvider>
+    </Suspense>
   </StrictMode>
 );
